2f14f729052f7ac9542b8cfe81bb6947b89e76a3
Jason Levinsohn <jason.levinsohn@gmail.com>
2015-06-08 10:48:18 -0400

    changed the name of the app from starter to l3

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('l3.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('l3.services', [])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
